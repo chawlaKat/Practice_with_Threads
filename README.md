26f7ef25e2d0ffae8a8074ecfc47a1d9bff98740
Kathy from Mocha <kchawla@uwyo.edu>
2019-08-17 19:31:25 -0600

    threads: add arguments, use thread container

--- README.md
+++ README.md
@@ -14,6 +14,8 @@ The file "practice_threads" contains a program that creates three threads and ha
 Uses the following reference pages:<br>
 * https://www.geeksforgeeks.org/multithreading-in-cpp/
 * https://www.geeksforgeeks.org/command-line-arguments-in-c-cpp/
+* https://stackoverflow.com/questions/10661792/how-to-create-an-array-of-thread-objects-in-c11
 
-Use "make" to compile the program.
-Then use "./practice_thread" to run it.
+Use "make" to compile the program.<br>
+Then use "./practice_thread" to run it. <br>
+To specify thread count, pass in a non-negative integer; for example, "./practice_thread 3"
--- practice_threads/main.cpp
+++ practice_threads/main.cpp
@@ -1,31 +1,51 @@
 #include <iostream>
 #include <thread>
+#include <vector>
 using namespace std;
 
 // thread identifies itself
 void sayHello(int id)
 {
-    string to_print = "I'm thread number " + to_string(id) + ".\n";
-    cout << to_print;
+    string hello = "Hi, I'm thread " + to_string(id) + ".\n";
+    cout << hello;
 }
 
-int main()
+int main(int argc, char *argv[])
 {
+    // desired number of threads
+    int requestThdCt = 0;
+    int threadCount = 5;
+
     // mark program start
-    cout << "Main started.\n";
+    cout << "Main started with " << argc << " arguments.\n";
+    
+    // if number of threads is specified, report it.
+    if (argc > 1)
+    {
+        cout << "You requested " << argv[1] << " thread(s).\n";
+
+        //TODO: add input validation, save to requestThdCt
+    }
+
+    // tell user the real thread count
+    cout << "Thread count: " << threadCount << "\n\n";
 
     // start some threads
-    thread th1(sayHello, 1);
-    thread th2(sayHello, 2);
-    thread th3(sayHello, 3);
+    thread thArray[threadCount];
+
+    for (int i = 0; i < threadCount; i++)
+    {
+        thArray[i] = thread(sayHello, i);
+    }
 
     // wait for threads to finish
-    th1.join();
-    th2.join();
-    th3.join();
+    for (int i = 0; i < threadCount; i++)
+    {
+        thArray[i].join();
+    }
 
     // mark program completion
-    cout << "Main completed.\n";
+    cout << "\nMain completed.\n";
 
     return 0;
 }
