8f055a1e76a041cb634ba56b5a1114d820baca82
Kathy from Mocha <kchawla@uwyo.edu>
2019-08-24 19:46:10 -0600

    iterate directory recursively, start searching for .txt

--- filesystem_practice/main.cpp
+++ filesystem_practice/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <experimental/filesystem>
+using namespace std;
 namespace fs = std::experimental::filesystem;
 
 //parameter: file to search
@@ -11,12 +13,14 @@ int main(int argc, char *argv[])
     fs::path pathToValidate;
     bool inputIsPath = false;
     bool inputPathExists = false;
+    int txtExtensionFoundAt = -1;
+    string pathFoundName = "";
 
     // check for argument
     if (argc > 1)
     {
-        std::cout << "hey look argument\n";
-        std::cout << "it's " << argv[1] << "\n";
+        cout << "hey look argument\n";
+        cout << "it's " << argv[1] << "\n";
 
         // check if arg is valid directory
         pathToValidate = fs::path(argv[1]);
@@ -31,16 +35,28 @@ int main(int argc, char *argv[])
     }
 
     // sample print
-    std::cout << "Input was: " << pathToValidate << "\n";
-    std::cout << "Path to search is: " << pathToSearch << "\n";
-    std::cout << "Current path is: " << fs::current_path() << "\n\n";
+    cout << "Input was: " << pathToValidate << "\n";
+    cout << "Path to search is: " << pathToSearch << "\n";
+    cout << "Current path is: " << fs::current_path() << "\n\n";
 
 
     // iterate through directory and all subdirectories
     // NOTE: if cycles exist, end iterator may not be reachable
-    for (auto pathFound : fs::recursive_directory_iterator(pathToSearch)){
-        std::cout << pathFound << "\n";
+    for (fs::path pathFound : fs::recursive_directory_iterator(pathToSearch)){
+        txtExtensionFoundAt = -1;
 
+        cout << pathFound << "\n";
+         
+        // check if is text file
+        pathFoundName = pathFound.string();
+        // txtExtensionFoundAt = pathFoundName.find(".txt")
+
+        // path contains ".txt"
+        // NOTE: would not handle ".txt.zip" properly yet
+        // if (txtExtensionFoundAt > 0)
+        // {
+        //     cout << pathFound << " is a .txt file\n\n";
+        // }
     }
 
     return 0;
