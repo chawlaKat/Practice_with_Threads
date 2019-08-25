7b645274a185dcb65d9ed7c170bd2ef033a30e7a
Kathy from Mocha <kchawla@uwyo.edu>
2019-08-25 12:30:31 -0600

    open file, start reading

--- parsing_practice/main.cpp
+++ parsing_practice/main.cpp
@@ -1,9 +1,54 @@
+/*
+* Okay! What do I need this to do?
+
+* - open a file (from input?)
+* - for each character, if a-z, A-Z, or 0-9, store it
+*    - if other (non-space), ignore it
+*    - if space, save stored word
+
+(shhh that's MOre than enough to start with. open a text file. start there.)
+
+*/
+
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    cout << "hello world!\n";
+    string filename = "sample_text_doc.txt";
+    string strInput = "";
+
+    // open a file
+    ifstream myFile(filename);
+
+    // catch error
+    // NOTE: try/catch block instead? is that just java?
+    if (!myFile)
+    {
+        cout << "Could not open " << filename << "\n";
+    }
+    else
+    {
+        cout << filename << " opened.\n";
+
+        // while there's still stuff in the file
+        // TODO: fix bug. reads "<3" twice.
+        while (myFile)
+        {
+            // reeead : )
+            // NOTE: extraction operator breaks on whitespace, not just spaces
+            myFile >> strInput;
+            cout << strInput << endl;
+        }
+
+        // close it!
+        // (does destructor already get called? does this cause problems?)
+        myFile.close();
+    }
+    
+
+    cout << "End of program.\n";
 
     return 0;
 }
